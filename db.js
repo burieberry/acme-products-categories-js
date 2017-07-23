f5533adcb67614bcfedcdad7a7c0ba5219583d51
Burcu Noyan <burcunoyan@gmail.com>
2017-07-23 15:00:25 -0400

    add initial data

---
+++ db.js
@@ -0,0 +1,22 @@
+var categories = {
+  "Foo Category": [
+    {
+      "name": "foo 1",
+      "id": 1
+    },
+    {
+      "name": "foo 2",
+      "id": 2
+    }
+  ],
+  "Bar Category": [
+    {
+      "name": "bar 1",
+      "id": 1
+    },
+    {
+      "name": "bar 2",
+      "id": 2
+    }
+  ]
+};
