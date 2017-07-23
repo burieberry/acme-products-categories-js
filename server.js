58eb8a83156efb4a2bb4ea8ebe5ea96c9e692314
Burcu Noyan <burcunoyan@gmail.com>
2017-07-23 14:29:15 -0400

    set up express, nunjucks, port

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+app.set('view engine', 'html')
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+const port = 3000 || process.env.PORT;
+app.listen(port, function(req, res, next) {
+  console.log(`Listening on port ${port}`);
+});
