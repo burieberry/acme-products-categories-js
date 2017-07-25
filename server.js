8c29b3f8c48a396d83949f4a2c11b5da91eb7e8b
Burcu Noyan <burcunoyan@gmail.com>
2017-07-25 00:04:41 -0400

    add error page and statusCode logging

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ nunjucks.configure('views', { noCache: true });
 app.use(bodyParser.urlencoded({ exdended: false }));
 app.use(methodOverride('_method'));
 
-app.use(function(req, res, next) {
+app.use('/', function(req, res, next) {
   console.log(req.method, req.url);
+  res.on('finish', function() {
+    console.log(req.method, req.url, res.statusCode);
+  });
   next();
 });
 
@@ -23,13 +26,20 @@ app.get('/', function(req, res, next) {
 
 app.use('/categories', require('./routes/categories'));
 
-const port = 3000 || process.env.PORT;
+app.use(function(err, req, res, next) {
+  res.render('error', { error: err });
+});
+
+app.use(function(req, res, next) {
+  res.render('error');
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, function(req, res, next) {
   console.log(`Listening on port ${port}`);
 });
 
 // TODO:
 // - active tab state
-// - update product functionality?
-// - error page & 404 handling
+// - "update product" functionality?
 
